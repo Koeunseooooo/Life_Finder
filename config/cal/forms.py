b63f9bd288b82d7f68432268fe363e695b726df3
koeunseo <rhdmstj17@cau.ac.kr>
2020-08-08 11:48:28 +0900

    master_sorry

--- config/cal/forms.py
+++ config/cal/forms.py
@@ -1,17 +1,22 @@
 from django.forms import ModelForm, DateInput
 from cal.models import Event
-
+from .widgets import RateitjsWidget
 
 class EventForm(ModelForm):
+
     class Meta:
         model = Event
         # datetime-local is a HTML5 input type, format to make date time show on fields
         widgets = {
-            'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
-        }
-        fields = '__all__'
+            'rating': RateitjsWidget,
+            # 'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
 
-    def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)
-        # input_formats parses HTML5 datetime-local input to datetime field
-        self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
+        }
+
+        fields = ['title','start_time','rating']
+        # , 'profile'
+
+    # def __init__(self, *args, **kwargs):
+    #     super(EventForm, self).__init__(*args, **kwargs)
+    #     # input_formats parses HTML5 datetime-local input to datetime field
+    #     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
--- config/config/settings.py
+++ config/config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'create_profile',
     'dashboard',
 
+    'django.forms',
+
 
 ]
 
@@ -126,11 +128,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'main','static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR,'main','static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
+
+#
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
