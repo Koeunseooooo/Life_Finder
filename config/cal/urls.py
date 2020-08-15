5ad60f72d8dc4c0720b588963d60c71cabb34574
koeunseo <rhdmstj17@cau.ac.kr>
2020-08-15 19:14:42 +0900

    '0815eunseo'

--- config/cal/urls.py
+++ config/cal/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
  path('event/edit/<int:event_id>',views.event_edit, name='event_edit'),
  # url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
  path('dash/',views.dash,name='dash'),
+ path('dash_detail/',views.dash_detail,name='dash_detail'),
  path('no_dash/',views.dash,name='no_dash'),
  # path('event/edit/<int:pk>/delete/', views.delete, name='delete'),
 
